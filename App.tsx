5f7878eabde42d9189035f1d210f9b2fdc5bce23
Sharvesh Patki <sspatki@dhcp-10-8-024-171.mobile.reshsg.uci.edu>
2024-10-23 14:35:40 -0700

    feat: add App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PaperProvider, MD3LightTheme } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import AppNavigator from "./src/navigation/AppNavigator";
+import { COLORS } from "./src/constants/colors";
+
+// Configure the Query Client
+const queryClient = new QueryClient();
 
 export default function App() {
+  // Create a custom theme extending MD3LightTheme
+  const theme = {
+    ...MD3LightTheme,
+    colors: {
+      ...MD3LightTheme.colors,
+      primary: COLORS.primary,
+      secondary: COLORS.secondary,
+      error: COLORS.error,
+    },
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <PaperProvider theme={theme}>
+          <AppNavigator />
+        </PaperProvider>
+      </QueryClientProvider>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
